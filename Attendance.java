976787d1476da821478c4801e912f91b2579deeb
FONTAG <lr.lffontanilla@mmdc.mcl.edu.ph>
2026-03-24 19:52:24 +0800

    Add Attendance class with employee details

--- Attendance.java
+++ Attendance.java
@@ -1,1 +1,26 @@
+public class Attendance {
 
+    private String employeeNumber;
+    private String date;
+    private String timeIn;
+    private String timeOut;
+
+    public Attendance(String empNum, String date, String in, String out) {
+        this.employeeNumber = empNum;
+        this.date = date;
+        this.timeIn = in;
+        this.timeOut = out;
+    }
+
+    public String getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public String getTimeIn() {
+        return timeIn;
+    }
+
+    public String getTimeOut() {
+        return timeOut;
+    }
+}
